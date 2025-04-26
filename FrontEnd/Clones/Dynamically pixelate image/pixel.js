9d5c3a27b476e38f38490f55bffd4b56268b00ad
Frederik Nielsen <fniel22@student.aau.dk>
2025-04-26 15:01:46 +0200

    changes to pixel

--- FrontEnd/Clones/Dynamically pixelate image/pixel.js
+++ FrontEnd/Clones/Dynamically pixelate image/pixel.js
@@ -1,22 +1,29 @@
+// pixel.js
+
 var img = new Image();
 img.crossOrigin = 'anonymous';
-var imagePath = 'Alien.jpg';
-img.src = imagePath;
+img.src = randomPoster;
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var level = 2;
+
+var scaleFactor = 0.5;
+
 img.onload = function () {
-    canvas.width = img.width;
-    canvas.height = img.height;
+    var scaledWidth = img.width * scaleFactor;
+    var scaledHeight = img.height * scaleFactor;
 
-    ctx.drawImage(img, 0, 0, img.width, img.height);
+    canvas.width = scaledWidth;
+    canvas.height = scaledHeight;
 
-    pixelateImage(ctx, canvas, level); // Level 1-5
+    ctx.drawImage(img, 0, 0, scaledWidth, scaledHeight);
+
+    let pixelationLevel = Math.max(1, 5 - attempts);
+    pixelateImage(ctx, canvas, pixelationLevel);
 };
 
 function pixelateImage(ctx, canvas, level) {
-    const blockSize = level * 10; // Adjust block size (higher level = larger blocks)
+    const blockSize = level * 5;
     const width = canvas.width;
     const height = canvas.height;
 
@@ -24,52 +31,46 @@ function pixelateImage(ctx, canvas, level) {
     const data = imageData.data;
 
     if (level > 0) {
-        
-    
-
-    // Loop through each block
-    for (let y = 0; y < height; y += blockSize) {
-        for (let x = 0; x < width; x += blockSize) {
-            let r = 0, g = 0, b = 0, count = 0;
-
-            // Loop through each pixel in the block
-            for (let yy = 0; yy < blockSize; yy++) {
-                for (let xx = 0; xx < blockSize; xx++) {
-                    let pixelX = x + xx;
-                    let pixelY = y + yy;
-                    if (pixelX < width && pixelY < height) { // Ensure within bounds
-                        let index = (pixelY * width + pixelX) * 4;
-                        r += data[index];
-                        g += data[index + 1];
-                        b += data[index + 2];
-                        count++;
+        for (let y = 0; y < height; y += blockSize) {
+            for (let x = 0; x < width; x += blockSize) {
+                let r = 0, g = 0, b = 0, count = 0;
+                for (let yy = 0; yy < blockSize; yy++) {
+                    for (let xx = 0; xx < blockSize; xx++) {
+                        let pixelX = x + xx;
+                        let pixelY = y + yy;
+                        if (pixelX < width && pixelY < height) {
+                            let index = (pixelY * width + pixelX) * 4;
+                            r += data[index];
+                            g += data[index + 1];
+                            b += data[index + 2];
+                            count++;
+                        }
                     }
                 }
-            }
 
-            // Compute average color for the block
-            r = Math.floor(r / count);
-            g = Math.floor(g / count);
-            b = Math.floor(b / count);
+                r = Math.floor(r / count);
+                g = Math.floor(g / count);
+                b = Math.floor(b / count);
 
-            // Assign average color to all pixels in the block
-            for (let yy = 0; yy < blockSize; yy++) {
-                for (let xx = 0; xx < blockSize; xx++) {
-                    let pixelX = x + xx;
-                    let pixelY = y + yy;
-                    if (pixelX < width && pixelY < height) {
-                        let index = (pixelY * width + pixelX) * 4;
-                        data[index] = r;
-                        data[index + 1] = g;
-                        data[index + 2] = b;
+                for (let yy = 0; yy < blockSize; yy++) {
+                    for (let xx = 0; xx < blockSize; xx++) {
+                        let pixelX = x + xx;
+                        let pixelY = y + yy;
+                        if (pixelX < width && pixelY < height) {
+                            let index = (pixelY * width + pixelX) * 4;
+                            data[index] = r;
+                            data[index + 1] = g;
+                            data[index + 2] = b;
+                        }
                     }
                 }
             }
         }
     }
-}
 
-    // Put modified image data back onto canvas
     ctx.putImageData(imageData, 0, 0);
 }
+
+
+
 
